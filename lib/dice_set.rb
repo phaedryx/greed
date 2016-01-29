e17e99baa442262bbe8f510e57d763017ca86b02
josh.richardson <jrichocean@gmail.com>
2016-01-29 16:24:36 -0700

    CREATES block in value method to make hash of values

--- lib/dice_set.rb
+++ lib/dice_set.rb
@@ -1,5 +1,17 @@
 class DiceSet
   def roll(number=5)
-    [6]
+    @values = Array.new(5){rand(6) + 1}
+  end
+
+
+  def score
+
+  end
+
+
+  def values values
+    h = Hash.new(0)
+    @values.each {|v| h.store(v, h[v]+1)}
+    return h
   end
 end
